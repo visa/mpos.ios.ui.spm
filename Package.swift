4c28effd54252dc06234d8435269e38d33f2e1dd
svc_munich-lab-ci <remgroup@visa.com>
2023-05-11 09:06:31 +0200

    Update mpos.ios.ui version: 2.59.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "mpos_ui",
-                    url:"https://repo.visa.com/mpos-releases/io/payworks/mpos.ios.ui/2.59.0/mpos.ios.ui-2.59.0.zip",
-                    checksum: "0f448275474690351e06a77468032eceb2719cc48ff408578969fecc415ef070"
+                    url:"https://repo.visa.com/mpos-releases/io/payworks/mpos.ios.ui/2.59.1/mpos.ios.ui-2.59.1.zip",
+                    checksum: "311caa1e803ea6ceda5ef4c666f214ec3245bdd51046b7ae2e42b25e00b2a429"
             ),
     ]
 )
